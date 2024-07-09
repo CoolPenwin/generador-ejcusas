aa97ca8813f2d2507a9f42fa0e88072d841ad544
CoolPenwin <coolPenwin@proton.me>
2024-07-09 12:00:40 +0000

    abuela chek funciona!

--- src/app.js
+++ src/app.js
@@ -6,14 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  //write your code here
   document.querySelector("#reload").addEventListener("click", () => {
-    //let randomNumber = Math.random() * 10;
     document.querySelector("#ejcusa").innerHTML = ejcusegenerador();
   });
   document.querySelector("#ejcusa").innerHTML = ejcusegenerador();
   console.log("Hello Rigo ejcuseme on the console too!");
 };
+var abuelaCheck = false; // Definimos la variable global
+
+// Seleccionamos el checkbox
+var checkbox = document.getElementById("abuela");
+
+// Agregamos el event listener
+checkbox.addEventListener("change", function() {
+  abuelaCheck = checkbox.checked; // Cambiamos el valor de la variable global según el estado del checkbox
+  console.log("abuelaCheck es ahora:", abuelaCheck); // Para verificar el cambio
+});
+
 let ejcusegenerador = () => {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
@@ -30,6 +39,10 @@ let ejcusegenerador = () => {
   let actionIndex = Math.floor(Math.random() * action.length);
   let whatIndex = Math.floor(Math.random() * what.length);
   let whenIndex = Math.floor(Math.random() * when.length);
+
+  if (abuelaCheck) {
+    whoIndex = 1;
+  }
 
   return (
     who[whoIndex] +
